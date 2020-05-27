408543bfaaf29410781d2148c9bec9e76e7bdb4f
Jason Lavoie <jlavoie@bowdoin.edu>
2020-05-27 09:20:32 -0400

    add example log output

--- getfailed.pl
+++ getfailed.pl
@@ -1,6 +1,11 @@
 #!/usr/bin/perl
 #
 # parse system log for list of filenames that failed to import
+#
+# Example log output:
+#Timestamp                       Thread     Type        Activity             PID    TTL
+#2020-05-22 11:40:28.019783-0400 0x2e8f3    Error       0x6f2ea              10623  0    Photos: (Photos) [com.apple.Photos:import.error] ERROR:  (Unsupported resource set: video,xmp, path: MVI_3051.avi), file: PHImportAsset.m, line: 1790
+#[...]
 
 use strict;
 
